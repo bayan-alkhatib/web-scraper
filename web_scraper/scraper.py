5910f700aca08a6620f80303f81c49a8fce1553a
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-06-28 22:16:19 +0300

    complete lab 17

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 URL= "https://en.wikipedia.org/wiki/History_of_Mexico"
 
-def  get_citations_needed_count():
+def  get_citations_needed_count(URL):
 
     page= requests.get(URL)
 
@@ -20,15 +20,28 @@ def  get_citations_needed_count():
 
 def get_citations_needed_report(URL):
     page = requests.get(URL)
+
     soup = BeautifulSoup(page.content, 'html.parser')
+
     results = soup.find(class_="mw-parser-output")
 
-    p_tags = []
+    all_results = results.findAll('p')
+    citation_p=[]
+    for i in all_results:
+        citation=i.findAll('a',title='Wikipedia:Citation needed')
+        for j in citation:
+            citation_p.append(i.text.strip()) 
+    return "\n".join(citation_p)
+
+
+
+
    
 
 if __name__=='__main__':
 
-    get_citations_needed_count()
+    get_citations_needed_count(URL)
+    print(get_citations_needed_report(URL))
     
 
    
